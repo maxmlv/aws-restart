da23ff1272f4407b1f4b63feee8418ef61c1686b
Maksym Malieiev <maxmlv@maxmlv.localdomain>
2022-10-28 13:29:46 +0300

    initial commit

---
+++ python-labs/lab2/numeric-data.py
@@ -0,0 +1,36 @@
+print(2+2)
+print(4-2)
+print(2*2)
+print(4/2)
+
+print("Python has three numeric types: int, float, and complex")
+
+# int
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# float
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# complex
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# boolean
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# boolean
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
